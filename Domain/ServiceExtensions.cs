0ae073057f0bf3a45e0cb9ca5d6135b2edf64025
VuHuuDuc18 <nhamayducanh@gmail.com>
2025-06-20 01:46:27 +0700

    cf

--- Domain/ServiceExtensions.cs
+++ Domain/ServiceExtensions.cs
@@ -1,5 +1,4 @@
-﻿using Domain.Services.Interfaces;
-using Entities.EntityModel;
+﻿using Domain.Services;
 using Domain.Services.Implements;
 using Domain.Services.Interfaces;
 using Domain.Settings;
@@ -11,18 +10,21 @@ using Infrastructure.Services.Implements;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-namespace Domain
+namespace Infrastructure
 {
     public static class ServiceExtensions
     {
-
         public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
-            //Add service
+            //Add DBContext
+            services.AddDbContext<LCFMSDBContext>(options =>
+               options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")),
+               ServiceLifetime.Transient);
+            services.AddScoped<DbContext, LCFMSDBContext>();
+
             //Add Cloudiary Config to DI Container
             services.Configure<CloudinaryConfig>(configuration.GetSection("Cloudinary"));
             //Register services
-            services.AddTransient<IBarnPlanService, BarnPlanService>();
             services.AddTransient<IUserService, UserService>();
             services.AddScoped<IBarnService, BarnService>();
             services.AddScoped<IFoodService, FoodService>();
@@ -32,8 +34,9 @@ namespace Domain
             services.AddScoped<IBreedCategoryService, BreedCategoryService>();
             services.AddScoped<IMedicineCategoryService, MedicineCategoryService>();
             services.AddScoped<IBreedService, BreedService>();
-            services.AddScoped<IUserService, UserService>();                
+            services.AddScoped<IEmailService, EmailService>();
             services.AddScoped<IDailyReportService, DailyReportService>();
+            services.AddScoped<IBillService, BillService>();
             services.AddScoped<CloudinaryCloudService>();
 
             //Add repo
@@ -41,8 +44,9 @@ namespace Domain
             services.AddScoped<IRepository<BarnPlanFood>, Repository<BarnPlanFood>>();
             services.AddScoped<IRepository<BarnPlanMedicine>, Repository<BarnPlanMedicine>>();
             services.AddScoped<IRepository<Role>, Repository<Role>>();
+            services.AddScoped<IRepository<Bill>, Repository<Bill>>();
+            services.AddScoped<IRepository<BillItem>, Repository<BillItem>>();
             services.AddScoped<IRepository<User>, Repository<User>>();
-            services.AddScoped<IRepository<Role>, Repository<Role>>();
             services.AddScoped<IRepository<Barn>, Repository<Barn>>();
             services.AddScoped<IRepository<Food>, Repository<Food>>();
             services.AddScoped<IRepository<Medicine>, Repository<Medicine>>();
@@ -52,7 +56,6 @@ namespace Domain
             services.AddScoped<IRepository<FoodCategory>, Repository<FoodCategory>>();
             services.AddScoped<IRepository<BreedCategory>, Repository<BreedCategory>>();
             services.AddScoped<IRepository<Breed>, Repository<Breed>>();
-            services.AddScoped<IRepository<User>, Repository<User>>();
             services.AddScoped<IRepository<ImageBreed>, Repository<ImageBreed>>();
             services.AddScoped<IRepository<MedicineCategory>, Repository<MedicineCategory>>();
             services.AddScoped<IRepository<LivestockCircle>, Repository<LivestockCircle>>();
@@ -61,8 +64,8 @@ namespace Domain
             services.AddScoped<IRepository<LivestockCircleFood>, Repository<LivestockCircleFood>>();
             services.AddScoped<IRepository<LivestockCircleMedicine>, Repository<LivestockCircleMedicine>>();
             services.AddScoped<IRepository<DailyReport>, Repository<DailyReport>>();
-            services.AddScoped<IRepository<DailyReport>, Repository<DailyReport>>();
 
         }
+
     }
 }
