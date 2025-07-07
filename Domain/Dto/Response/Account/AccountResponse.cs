df25c378bd680090d8dbec7614dfde324af2f762
NHT <thai03040302@gmail.com>
2025-07-07 17:00:55 +0700

    edit account list response

--- Domain/Dto/Response/Account/AccountResponse.cs
+++ Domain/Dto/Response/Account/AccountResponse.cs
@@ -9,8 +9,14 @@ namespace Domain.Dto.Response.Account
     public class AccountResponse
     {
         public Guid Id { get; set; }
+        public string Email { get; set; }
         public string FullName { get; set; }
         public bool IsActive { get; set; }
+        public string PhoneNumber{ get; set; }
         public string RoleName { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public Guid? UpdatedBy { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
     }
 }
