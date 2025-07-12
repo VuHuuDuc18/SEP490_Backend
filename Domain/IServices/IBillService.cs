b5cb8da3446bb164d23ba107c0dc2aab3ea87548
duy10102003 <duya1k20nl4@gmail.com>
2025-07-12 13:50:44 +0700

    Fix bill api

--- Domain/IServices/IBillService.cs
+++ Domain/IServices/IBillService.cs
@@ -26,6 +26,7 @@ namespace Domain.IServices
         Task<(PaginationSet<BillItemResponse> Result, string ErrorMessage)> GetBillItemsByBillId(Guid billId, ListingRequest request, CancellationToken cancellationToken = default);
         Task<(BillResponse Bill, string ErrorMessage)> GetBillById(Guid billId, CancellationToken cancellationToken = default);
         Task<(PaginationSet<BillResponse> Result, string ErrorMessage)> GetPaginatedBillList(ListingRequest request, CancellationToken cancellationToken = default);
+        Task<(PaginationSet<BillResponse> Result, string ErrorMessage)> GetPaginatedBillListHistory(ListingRequest request, string billType, CancellationToken cancellationToken = default);
         Task<(PaginationSet<BillResponse> Result, string ErrorMessage)> GetBillRequestByType(ListingRequest request, string billType, CancellationToken cancellationToken = default);
         Task<(bool Success, string ErrorMessage)> ChangeBillStatus(Guid billId, string newStatus, CancellationToken cancellationToken = default);
         Task<(bool Success, string ErrorMessage)> AddFoodItemToBill(Guid billId, AddFoodItemToBillDto request, CancellationToken cancellationToken = default);
@@ -43,6 +44,11 @@ namespace Domain.IServices
         Task<(bool Success, string ErrorMessage)> UpdateBillFood(Guid billId, UpdateBillFoodDto request, CancellationToken cancellationToken = default);
         Task<(bool Success, string ErrorMessage)> UpdateBillMedicine(Guid billId, UpdateBillMedicineDto request, CancellationToken cancellationToken = default);
         Task<(bool Success, string ErrorMessage)> UpdateBillBreed(Guid billId, UpdateBillBreedDto request, CancellationToken cancellationToken = default);
+
+        public Task<(PaginationSet<BillResponse> Result, string ErrorMessage)> GetApprovedBillsByWorker(ListingRequest request, CancellationToken cancellationToken = default);
+        public Task<(PaginationSet<BillResponse> Result, string ErrorMessage)> GetHistoryBillsByWorker(ListingRequest request, CancellationToken cancellationToken = default);
+
+        Task<(PaginationSet<BillResponse> Result, string ErrorMessage)> GetPaginatedBillListByTechicalStaff(ListingRequest request, string billType, CancellationToken cancellationToken = default);
         public Task<bool> AdminUpdateBill(Admin_UpdateBarnRequest request);
     }
 }
