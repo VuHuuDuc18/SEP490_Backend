9b41a25030e9a6417a0ccca67b88b14ea42f3658
NHT <thai03040302@gmail.com>
2025-08-11 16:33:22 +0700

    update account services
    - change role constants from static to const
    - add query new password to api

--- Domain/Helper/Constants/RoleConstant.cs
+++ Domain/Helper/Constants/RoleConstant.cs
@@ -8,13 +8,13 @@ namespace Domain.Helper.Constants
 {
     public static class RoleConstant
     {        
-        public static string CompanyAdmin = "Company Admin";
-        public static string TechnicalStaff = "Technical Staff";
-        public static string FeedRoomStaff = "Feed Room Staff";
-        public static string MedicineRoomStaff = "Medicine Room Staff";
-        public static string BreedingRoomStaff = "Breeding Room Staff";
-        public static string Worker = "Worker";
-        public static string SalesStaff = "Sales Staff";
-        public static string Customer = "Customer";
+        public const string CompanyAdmin = "Company Admin";
+        public const string TechnicalStaff = "Technical Staff";
+        public const string FeedRoomStaff = "Feed Room Staff";
+        public const string MedicineRoomStaff = "Medicine Room Staff";
+        public const string BreedingRoomStaff = "Breeding Room Staff";
+        public const string Worker = "Worker";
+        public const string SalesStaff = "Sales Staff";
+        public const string Customer = "Customer";
     }
 }
